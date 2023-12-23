a8ff4d16321f1d7a75812f8f50c1c8276ca20439
amryyahya <amryy9@gmail.com>
2023-12-23 10:17:39 +0700

    fix build.sh variable name

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ docker images
 NEW_IMAGE_NAME="ghcr.io/$GITHUB_USERNAME/$IMAGE_NAME:$IMAGE_TAG"
 docker tag "$IMAGE_NAME:$IMAGE_TAG" "$NEW_IMAGE_NAME"
 
-echo $PAT_GITHUB | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
+echo $GITHUB_PAT | docker login ghcr.io -u $GITHUB_USERNAME --password-stdin
 
 docker push "$NEW_IMAGE_NAME"
