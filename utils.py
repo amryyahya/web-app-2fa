bdfbcf745fffdfb1c52252f050cdc4fad70868d0
amryyahya <amryy9@gmail.com>
2024-06-23 12:28:27 +0000

    add PyJWT

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import hashlib, qrcode, io, os, base64, string, random, datetime, ctypes
+import hashlib, qrcode, io, os, base64, string, random, datetime, ctypes,jwt
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import padding
