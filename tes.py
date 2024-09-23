6f8bdaf7bda1e2bedae418ffc98a7f725bac8ea5
Amry Yahya <90252267+amryyahya@users.noreply.github.com>
2024-09-23 07:43:02 +0700

    organizing some files

--- tes.py
+++
@@ -1,8 +0,0 @@
-import jpype
-
-jpype.startJVM(classpath=['.'])
-TOTPGenerator = jpype.JClass('TOTP')
-secret_key = "gbOJmc8at8frcro1bV8MxD2ChcIg99ZV"
-time = 1719885656
-totp = TOTPGenerator.TOTP(secret_key,time)
-print(totp)
